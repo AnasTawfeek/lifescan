b6bafd4b061165ea3eed363c57d6a62ea271de8e
tonyseing <tony.seing@gmail.com>
2013-06-04 23:41:31 -0400

    start page OK

--- new_verio/start/res/javascript/1.0.js
+++ new_verio/start/res/javascript/1.0.js
@@ -1,44 +1,59 @@
 $(document).ready(function(){
-
+        animate_text_appear();
 	function jump(page)
 	{
+		$('.animationLayer').fadeOut();
 		$('.contentLayer').fadeOut( function(){				  					
 			document.location = page;			
 		});	
 	};
 	
-	$('.nav').live('touchstart click', function(e){
+		$('#face1').live('touchstart click', animateFace);
+		$('#face2').live('touchstart click', animateFace);
+		$('#face3').live('touchstart click', animateFace);
+		$('#face4').live('touchstart click', animateFace);
+		$('#face5').live('touchstart click', animateFace);
+		$('#face6').live('touchstart click', animateFace);
+
+	function animateFace(e) {
 		e.stopPropagation(); e.preventDefault();
-		var btn = $(this);
-		var page;
-		
-		if(btn.hasClass('prev'))
-			page = "start.html";
+		if($(this).hasClass('flip')) {
+			$(this).removeClass('flip');
+		}
 		else
-			page = "veeva:gotoSlide(home.zip)";
-		
-		jump(page);			
-	});
-	
-	$('.card').live('touchstart click', function(e){
-		e.stopPropagation(); e.preventDefault();
-		
-		$('.button').live('touchstart click', function(e){
-			e.stopPropagation(); e.preventDefault();
-			
-			jump("veeva:gotoSlide(home.zip)");					
-		});
-		
-		$('.click').addClass('flip');
-		
-		move("#card-container")
-			.ease('in-out')
-			.duration('1.1s')
-			.x(-215)
-			.end();					
-	});
+		{
+			$(this).addClass('flip');
+		}
+	}
+
+       function animate_text_appear() {
+           // animates text onto screen one line at a time
+           setTimeout(function() { $('.intro1').fadeIn(); }, 1000);
+           setTimeout(function() { $('.intro2').fadeIn(); }, 1400);
+           setTimeout(function() { $('.intro3').fadeIn(); }, 1800);
+           setTimeout(function() { $('.intro4').fadeIn(); }, 2200);
+           
+       }
+    
+       $('.popup_btn').click(function() {
+           $('.overlay').show();
+       });
+
+       $('.overlay').click(function() {
+          $('.overlay').hide();
+       });
+
+
+
 	
 	// fade in the content...
-	$('.contentLayer').fadeIn();
+//	$('.contentLayer').fadeIn(function(){
+//		$('.animationLayer').fadeIn('slow');
+//	});
+	
+	// fade in the content...
+	$('#rightside').click(function(e) {
+   		$('#stats').addClass("showMe");     
+    });
 
 });
