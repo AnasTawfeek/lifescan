2a453c14e3b11007a9b264a733cab09beab89c3a
tony <tony@tony-VirtualBox.(none)>
2013-06-06 21:11:17 -0400

    verio conversion

--- new_verio/veriosync/res/javascript/1.0.js
+++ new_verio/veriosync/res/javascript/1.0.js
@@ -1,41 +1,41 @@
 $(document).ready(function(){
 
-	function jump(page)
-	{
-		$('.animationLayer').fadeOut();
-		$('.contentLayer').fadeOut( function(){				  					
-			document.location = page;			
-		});	
-	};
-	
-		$('#face1').live('touchstart click', animateFace);
-		$('#face2').live('touchstart click', animateFace);
-		$('#face3').live('touchstart click', animateFace);
-		$('#face4').live('touchstart click', animateFace);
-		$('#face5').live('touchstart click', animateFace);
-		$('#face6').live('touchstart click', animateFace);
+  function jump(page)
+  {
+    $('.animationLayer').fadeOut();
+    $('.contentLayer').fadeOut( function(){				  					
+      document.location = page;			
+    });	
+  };
+  
+  $('#face1').live('touchstart click', animateFace);
+  $('#face2').live('touchstart click', animateFace);
+  $('#face3').live('touchstart click', animateFace);
+  $('#face4').live('touchstart click', animateFace);
+  $('#face5').live('touchstart click', animateFace);
+  $('#face6').live('touchstart click', animateFace);
 
-	function animateFace(e) {
-		e.stopPropagation(); e.preventDefault();
-		if($(this).hasClass('flip')) {
-			$(this).removeClass('flip');
-		}
-		else
-		{
-			$(this).addClass('flip');
-		}
-	}
+  function animateFace(e) {
+    e.stopPropagation(); e.preventDefault();
+    if($(this).hasClass('flip')) {
+      $(this).removeClass('flip');
+    }
+    else
+    {
+      $(this).addClass('flip');
+    }
+  }
 
 
-	
-	// fade in the content...
-//	$('.contentLayer').fadeIn(function(){
-//		$('.animationLayer').fadeIn('slow');
-//	});
-	
-	// fade in the content...
-	$('#rightside').click(function(e) {
-   		$('#stats').addClass("showMe");     
-    });
+  
+  // fade in the content...
+  //	$('.contentLayer').fadeIn(function(){
+  //		$('.animationLayer').fadeIn('slow');
+  //	});
+  
+  // fade in the content...
+  $('#rightside').click(function(e) {
+    $('#stats').addClass("showMe");     
+  });
 
-});
+;})
