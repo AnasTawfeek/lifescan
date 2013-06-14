7ce69a35e54ac430652c695b6d557e5dbe5ef26f
tony <tony@tony-VirtualBox.(none)>
2013-06-13 21:05:31 -0400

    adding global links for different sections

---
+++ salesaid/veriosync_start/res/javascript/2.0.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+	window.jump=function(page)
+	{
+		console.log(page);
+		$('.animationLayer').fadeOut();
+		$('.contentLayer').fadeOut( function(){				  					
+			document.location = page;			
+		});	
+	};
+	
+	$('#videoThumb img').live('touchstart click', function(e){
+		$("#videoThumb").hide();
+		$("#video").get(0).play();
+	});
+	
+    var video = document.getElementsByTagName('video')[0];
+	video.addEventListener("ended", function () {
+      $("#copy").fadeTo(2000,1);
+	}, false);
+});
