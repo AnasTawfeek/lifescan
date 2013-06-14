8b984a85d1304d54c282280fbb1172bf0caffeb6
tony <tony@tony-VirtualBox.(none)>
2013-06-13 21:26:22 -0400

    trying adding presentation ids to links

---
+++ salesaid/verioplatform_veriosync/res/javascript/3.0.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+	window.jump=function(page)
+	{
+		$('.animationLayer').fadeOut();
+		$('.contentLayer').fadeOut( function(){				  					
+			document.location = page;			
+		});	
+		
+	};
+	
+	var _iMenu=0;
+	var lastItem=$("#phone_none");
+	
+	window.showMenu=function(iMenu) {
+		if(iMenu==_iMenu) return false;
+		_iMenu=iMenu;
+		
+	
+		for(var i=1;i<=3;i++)
+			if(i==_iMenu)
+				$("#menu"+i).show();
+			else
+					$("#menu"+i).hide();
+		
+		if(lastItem) 
+			$(lastItem).fadeOut(600);
+		
+		lastItem=$("#phone_"+_iMenu);
+		$(lastItem).fadeIn(600);
+		
+			
+		return false;
+	}
+});
