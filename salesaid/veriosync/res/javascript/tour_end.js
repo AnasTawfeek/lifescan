0734b22b146c515b570e18831bf0106f73a63f1d
Markus Winkler <contact@markuswinkler.com>
2013-07-18 20:00:28 -0400

    versiosync

--- salesaid/veriosync/res/javascript/tour_end.js
+++ salesaid/veriosync/res/javascript/tour_end.js
@@ -6,9 +6,9 @@ $(document).ready(function(){
     var page;
     
     if(btn.hasClass('prev'))
-      page = "slide-2.0.html";
+      page = "tour.html";
     else
-      page = "tour_end.html";
+      page = "slide-4.0.html";
     
     jump(page);			
   });
