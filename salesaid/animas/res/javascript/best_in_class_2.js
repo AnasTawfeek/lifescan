ff3723e4417ab20b9c29972b44b8a27688d57eb9
Tony Seing <tony.seing@gmail.com>
2014-01-13 17:15:09 -0500

    adding new section to animas

---
+++ salesaid/animas/res/javascript/best_in_class_2.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  var imgpath = "res/images/performance_1/";
+  window.pagePrev = "best_in_class_1.html";
+  window.pageNext = "best_in_class_3.html";
+  
+   $(".briefsafety").click(function() {
+    $('.popup').toggle();
+  });
+  
+});
--- salesaid/animas/res/javascript/global.js
+++ salesaid/animas/res/javascript/global.js
@@ -1,74 +1,74 @@
 
 $(document).ready(function(){
-	
-	$('body').bind('touchmove', function (e) {
-		e.preventDefault();
-		return true; 
-	});	
-	
-	window.preload_pictures = function(picture_urls, aImg, callback)
+  
+  $('body').bind('touchmove', function (e) {
+    e.preventDefault();
+    return true; 
+  });	
+  
+  window.preload_pictures = function(picture_urls, aImg, callback)
+  {
+    var loaded  = 0;
+
+    for(var i = 0, j = picture_urls.length; i < j; i++)
+    {
+      var img = new Image();
+      img.onload  = function()
+      {                               
+	if(++loaded == picture_urls.length && callback)
 	{
-		var loaded  = 0;
+	  callback();
+	}
+      }
+      img.src = picture_urls[i];
+      aImg.push(img);
+    }
+  };
 
-		for(var i = 0, j = picture_urls.length; i < j; i++)
-   		{
-			var img = new Image();
-			img.onload  = function()
-			{                               
-				if(++loaded == picture_urls.length && callback)
-				{
-					callback();
-				}
-			}
-			img.src = picture_urls[i];
-			aImg.push(img);
-    	}
-	};
+  window.jump = function(page)
+  {
+    move('.contentLayer')
+      .duration('0.6s')
+      .set('opacity',0)
+      .end();
+    
+    move('.animationLayer')
+      .duration('0.6s')
+      .set('opacity',0)
+      .end(function(){
+	document.location = page;			
+      });
+  };
 
-	window.jump = function(page)
-	{
-		move('.contentLayer')
-		.duration('0.6s')
-		.set('opacity',0)
-		.end();
-	
-		move('.animationLayer')
-		.duration('0.6s')
-		.set('opacity',0)
-		.end(function(){
-			document.location = page;			
-		});
-	};
+  
+  var images = [ 
+    "res/images/global/btn-home-on.png" 
+  ];		
+  
+  var img1 = new Image();
+  img1.src = images[0];
+  //	var img2 = new Image();
+  //	img2.src = images[1];
+  //	var img3 = new Image();
+  //	img3.src = images[2];
 
-	
-	var images = [ 
-		"res/images/global/btn-home-on.png" 
-	];		
-	
-	var img1 = new Image();
-	img1.src = images[0];
-//	var img2 = new Image();
-//	img2.src = images[1];
-//	var img3 = new Image();
-//	img3.src = images[2];
-
-	// button setup...				
-	$("#btn-home").live('touchstart click', function(e){
-		e.stopPropagation(); e.preventDefault();
-	   	$(this).attr("src", images[0] );		
-	    window.jump("menu.html");
-	 });
-	 
-	$('.nav').live('touchstart click', function(e){
-		e.stopPropagation(); e.preventDefault();
-		var btn = $(this);
-		var page;
-		
-		if(btn.hasClass('prev'))
-			page = window.pagePrev;
-		else
-			page = window.pageNext;
-		
-		window.jump(page);			
-	});
+  // button setup...				
+  $("#btn-home").live('touchstart click', function(e){
+    e.stopPropagation(); e.preventDefault();
+    $(this).attr("src", images[0] );		
+    window.jump("menu.html");
+  });
+  
+  $('.nav').live('touchstart click', function(e){
+    e.stopPropagation(); e.preventDefault();
+    var btn = $(this);
+    var page;
+    
+    if(btn.hasClass('prev'))
+      page = window.pagePrev;
+    else
+      page = window.pageNext;
+    
+    window.jump(page);			
+  });
 });
